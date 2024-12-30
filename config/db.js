6bcd26da6d8dd8be7a6452720fd09b90e7575321
chocopndn <chocopndn@gmail.com>
2024-12-30 20:55:51 +0800

    ADD: Database connection

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection successful.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+module.exports = sequelize;
