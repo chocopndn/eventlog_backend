cea9e0b5b4d86351fd296759a8f58df012defcf3
chocopndn <chocopndn@gmail.com>
2025-01-12 08:55:56 +0800

    UPDATE: Method from GET to POST

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.route("/signup").post(authController.signup);
-router.route("/login").get(authController.login);
+router.route("/login").post(authController.login);
 router.route("/resetPassword").get(authController.resetPassword);
 router.route("/verifyResetCode").get(authController.verifyResetCode);
 
