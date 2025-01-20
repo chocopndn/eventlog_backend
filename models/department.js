80f6de2fca1a709e2b2e700af90ff25fa18e9c63
chocopndn <chocopndn@gmail.com>
2025-01-20 20:43:29 +0800

    UPDATE: Model

--- models/department.js
+++ models/department.js
@@ -1,17 +1,25 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  const Department = sequelize.define("Department", {
-    department_ID: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+  const Department = sequelize.define(
+    "Department",
+    {
+      department_ID: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      departmentName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
     },
-    departmentName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-  });
+    {
+      tableName: "department",
+      timestamps: false,
+    }
+  );
+
   return Department;
 };
