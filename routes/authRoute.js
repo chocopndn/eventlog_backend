7ab475563085d176335e48155c3f86630d54724f
chocopndn <chocopndn@gmail.com>
2025-01-23 17:58:33 +0800

    UPDATE: Changed from get to post

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,7 +5,7 @@ const authController = require("../controllers/authController");
 
 router.route("/signup").post(authController.signup);
 router.route("/login").post(authController.login);
-router.route("/resetPassword").get(authController.resetPassword);
-router.route("/verifyResetCode").get(authController.verifyResetCode);
+router.route("/resetPassword").post(authController.resetPassword);
+router.route("/verifyResetCode").post(authController.verifyResetCode);
 
 module.exports = router;
