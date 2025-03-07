ece74592e2cc608a89ddf13e9b866c91d050a58d
chocopndn <chocopndn@gmail.com>
2025-03-07 15:23:51 +0800

    Removed unnecessary code

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,11 +3,6 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.get("/department/:department_id", userController.getUsersByDepartment);
-router.get("/block/:block_id", userController.getUsersByBlock);
-router.get("/year-level/:yearlevel_id", userController.getUsersByYearLevel);
 router.post("/change-password", userController.changePassword);
 
 module.exports = router;
