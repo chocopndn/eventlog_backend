43069c3d537c9b0e04b9aab8ba0e95b56f954a77
chocopndn <chocopndn@gmail.com>
2025-03-18 10:31:32 +0800

    Add edit event endpoint

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -7,5 +7,6 @@ router.route("/user/upcoming").post(eventController.userUpcomingEvents);
 router.route("/user/attendance").post(eventController.recordAttendance);
 
 router.route("/admin/add").post(eventController.addEvent);
+router.route("/admin/edit").post(eventController.editEvent);
 
 module.exports = router;
