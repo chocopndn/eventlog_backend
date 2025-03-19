5d2e2a3509da7935c9c588341887394e76e818ce
chocopndn <chocopndn@gmail.com>
2025-03-20 00:56:23 +0800

    Cleanup code

--- controllers/blockController.js
+++ controllers/blockController.js
@@ -2,7 +2,7 @@ const { pool } = require("../config/db");
 
 exports.getAllBlocks = async (req, res) => {
   try {
-    const blocks = await pool.query("SELECT * FROM v_blocks");
+    const [blocks] = await pool.query("SELECT * FROM v_blocks");
     res.status(200).json({
       success: true,
       data: blocks,
@@ -29,14 +29,14 @@ exports.getBlocksByDepartment = async (req, res) => {
   }
 
   try {
-    const [rows] = await pool.query(
+    const [blocks] = await pool.query(
       "SELECT * FROM blocks WHERE department_id = ?",
       [departmentId]
     );
 
     res.status(200).json({
       success: true,
-      data: rows,
+      data: blocks,
       message: "Blocks fetched successfully.",
     });
   } catch (error) {
