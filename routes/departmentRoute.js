a9957d4235ffbf0649bfc446430b3145831428fc
chocopndn <chocopndn@gmail.com>
2025-04-02 01:50:07 +0800

    Fixed route

--- routes/departmentRoute.js
+++ routes/departmentRoute.js
@@ -7,6 +7,6 @@ router.route("/departments").get(departmentController.getDepartments);
 router.route("/departments/:id").get(departmentController.getDepartmentById);
 router.route("/departments").post(departmentController.addDepartment);
 router.route("/departments/:id").put(departmentController.updateDepartment);
-router.route("/departments/:id").delete(departmentController.deleteDepartment);
+router.route("/departments/del/:id").put(departmentController.deleteDepartment);
 
 module.exports = router;
