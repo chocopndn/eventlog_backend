de91129c52b6525923feddfb1f3305232c59bb7b
chocopndn <chocopndn@gmail.com>
2025-04-02 15:42:38 +0800

    Fixed route

--- routes/blockRoutes.js
+++ routes/blockRoutes.js
@@ -5,9 +5,7 @@ const blockController = require("../controllers/blockController");
 
 router.route("/").get(blockController.getAllBlocks);
 router.route("/").post(blockController.addBlock);
-router
-  .route("/departments/:departmentId")
-  .get(blockController.getBlocksByDepartment);
+router.route("/:departmentId").get(blockController.getBlocksByDepartment);
 router.route("/block/:id").get(blockController.getBlockById);
 router.route("/block/:id").put(blockController.editBlock);
 router.route("/:id").delete(blockController.deleteBlock);
