2fa0160166e0e6fbc128d8a01b90f709510c1501
chocopndn <chocopndn@gmail.com>
2025-04-11 20:25:34 +0800

    Added helper for archiving past events

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 const { pool } = require("./config/db");
+const cron = require("node-cron");
+const { archivePastEvents } = require("./helpers/archivePastEvents");
 
 console.log("Starting the server...");
 
@@ -10,8 +12,14 @@ console.log("Starting the server...");
     connection.release();
 
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
       console.log(`Server running on port ${PORT}`);
+
+      await archivePastEvents();
+    });
+
+    cron.schedule("0 0 * * *", async () => {
+      await archivePastEvents();
     });
   } catch (error) {
     console.error("Error connecting to the database:", error);
